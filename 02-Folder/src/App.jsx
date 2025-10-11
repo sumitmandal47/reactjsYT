a6708238cff8cdafd5802c8f21eb32bdefeadeed
sumit mandal <mandalsumit615@gmail.com>
2025-10-11 13:24:19 +0530

    folder second

---
+++ 02-Folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './components/Card';
+
+const App = () => {
+
+  const user="sumit"
+  return (
+    <>
+      <div className="card">
+        <Card />
+        <Card />
+        
+     
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ 02-Folder/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h1> hello sumit </h1>
+      <p>
+        lorem AI images at your fingertips — Imagine it. Style it. Refine it.
+        Whatever image you can think up, ChatGPT can generate. Experience the
+        power of AI with the official ChatGPT. Available for everyone for free.
+      </p>
+    </div>
+  );
+}
+
+export default Card
